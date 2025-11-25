835b84987448d6a9f8cd9843f15b14e6250b194e
Dylan Mora <damora@uamv.edu.ni>
2025-11-24 18:10:50 -0600

    BusquedaTexto
    Utilice la herramiento richtextbox en la cual el usuario puede poner el texto de su preferencia, este mismo se recorre para ver si la palabra que ingreso el usuario coincide.

---
+++ BusquedaTexto/BusquedaTexto/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace BusquedaTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbItem = new System.Windows.Forms.TextBox();
+            this.rtbParrafo = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(754, 65);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(125, 67);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Item a buscar:";
+            // 
+            // tbItem
+            // 
+            this.tbItem.Location = new System.Drawing.Point(199, 86);
+            this.tbItem.Name = "tbItem";
+            this.tbItem.Size = new System.Drawing.Size(510, 31);
+            this.tbItem.TabIndex = 2;
+            // 
+            // rtbParrafo
+            // 
+            this.rtbParrafo.Location = new System.Drawing.Point(12, 221);
+            this.rtbParrafo.Name = "rtbParrafo";
+            this.rtbParrafo.Size = new System.Drawing.Size(886, 532);
+            this.rtbParrafo.TabIndex = 3;
+            this.rtbParrafo.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(484, 37);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "INGRESE TU TEXTO AQUI:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 810);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtbParrafo);
+            this.Controls.Add(this.tbItem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBuscar);
+            this.Name = "Form1";
+            this.Text = "BusquedaTexto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbItem;
+        private System.Windows.Forms.RichTextBox rtbParrafo;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BusquedaTexto/BusquedaTexto/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusquedaTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string texto = rtbParrafo.Text.ToLower();
+            string palabra = tbItem.Text.ToLower();
+            int contador = 0;
+
+            if (string.IsNullOrEmpty(palabra) || palabra.Length > texto.Length) return;
+
+            
+            for (int i = 0; i <= texto.Length - palabra.Length; i++)
+            {
+                bool coincide = true;
+
+                
+                for (int j = 0; j < palabra.Length; j++)
+                {
+                    if (texto[i + j] != palabra[j])
+                    {
+                        coincide = false;
+                        break;
+                    }
+                }
+
+                if (coincide)
+                {
+                    contador++;
+                    
+                }
+            }
+
+            MessageBox.Show($"La palabra '{palabra}' aparece {contador} veces.");
+        }
+    }
+}
