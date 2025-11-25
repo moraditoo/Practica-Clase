e51fe477f134e39501b1cdfc55e906b7ccea633e
Dylan Mora <damora@uamv.edu.ni>
2025-11-24 19:11:10 -0600

    matriz

---
+++ BusquedaMatriz/BusquedaMatriz/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace BusquedaMatriz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMatriz = new System.Windows.Forms.DataGridView();
+            this.btnBuscarMatriz = new System.Windows.Forms.Button();
+            this.tbNumeroMatriz = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMatriz
+            // 
+            this.dgvMatriz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatriz.Location = new System.Drawing.Point(12, 223);
+            this.dgvMatriz.Name = "dgvMatriz";
+            this.dgvMatriz.RowHeadersWidth = 82;
+            this.dgvMatriz.RowTemplate.Height = 33;
+            this.dgvMatriz.Size = new System.Drawing.Size(1200, 574);
+            this.dgvMatriz.TabIndex = 0;
+            // 
+            // btnBuscarMatriz
+            // 
+            this.btnBuscarMatriz.Location = new System.Drawing.Point(135, 99);
+            this.btnBuscarMatriz.Name = "btnBuscarMatriz";
+            this.btnBuscarMatriz.Size = new System.Drawing.Size(157, 95);
+            this.btnBuscarMatriz.TabIndex = 1;
+            this.btnBuscarMatriz.Text = "BuscarMatriz";
+            this.btnBuscarMatriz.UseVisualStyleBackColor = true;
+            this.btnBuscarMatriz.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbNumeroMatriz
+            // 
+            this.tbNumeroMatriz.Location = new System.Drawing.Point(347, 125);
+            this.tbNumeroMatriz.Name = "tbNumeroMatriz";
+            this.tbNumeroMatriz.Size = new System.Drawing.Size(680, 31);
+            this.tbNumeroMatriz.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1240, 830);
+            this.Controls.Add(this.tbNumeroMatriz);
+            this.Controls.Add(this.btnBuscarMatriz);
+            this.Controls.Add(this.dgvMatriz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMatriz;
+        private System.Windows.Forms.Button btnBuscarMatriz;
+        private System.Windows.Forms.TextBox tbNumeroMatriz;
+    }
+}
+
---
+++ BusquedaMatriz/BusquedaMatriz/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BusquedaMatriz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LlenarMatriz();
+        }
+
+        int[,] matriz = new int[10, 10];
+
+
+        private void LlenarMatriz()
+        {
+            dgvMatriz.AllowUserToAddRows = false;
+            dgvMatriz.ColumnCount = 10;
+            dgvMatriz.RowCount = 10;
+
+            for (int col = 0; col < 10; col++)
+            {
+                dgvMatriz.Columns[col].Width = 60;
+                dgvMatriz.Columns[col].Name = col.ToString();
+            }
+
+            
+            Random rnd = new Random();
+
+            for (int f = 0; f < 10; f++)
+            {
+                for (int c = 0; c < 10; c++)
+                {
+                    matriz[f, c] = rnd.Next(1, 100);
+                    dgvMatriz.Rows[f].Cells[c].Value = matriz[f, c];
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(tbNumeroMatriz.Text, out int objetivo)) return;
+
+            string posiciones = "";
+            bool encontrado = false;
+
+
+            for (int f = 0; f < 10; f++)
+            {
+                for (int c = 0; c < 10; c++)
+                {
+                    if (matriz[f, c] == objetivo)
+                    {
+                        posiciones += $"[{f},{c}] ";
+                        encontrado = true;
+
+                    }
+                }
+            }
+
+            if (encontrado)
+                MessageBox.Show($"El número {objetivo} está en: {posiciones}");
+            else
+                MessageBox.Show("El número no está en la matriz.");
+        }
+    }
+}
